c4d9f24bc7f25bd20518d764328b87d72c0a6a2c
Aharon Petrosyan <petrosyan-ahar@mail.ru>
2025-07-20 20:43:41 +0400

    Add

--- new_f.c
+++ new_f.c
@@ -1,4 +1,17 @@
+void ft_putchar(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        write(1,&str[i],1);
+        i++;
+    }
+}
+
 int main(void)
 {
     printf("hello");
 }
+
